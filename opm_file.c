e3a14881cc3a9af031424fa5e8dd6b93473d695b
vampi <vampi@vampi.tech>
2023-05-07 12:44:46 +0300

    opm loader: load any number of voices

--- opm_file.c
+++ opm_file.c
@@ -4,39 +4,10 @@
 #include "opm_file.h"
 
 void opm_file_init(struct opm_file *f) {
-	for(int i = 0; i < OPM_FILE_MAX_VOICES; i++) {
-		struct opm_file_voice *v = f->voices + i;
-		v->lfo_lfrq = 0;
-		v->lfo_amd = 0;
-		v->lfo_pmd = 0;
-		v->lfo_wf = 0;
-		v->nfrq = 0;
-		v->ch_pan = 64;
-		v->ch_fl = 7;
-		v->ch_con = 4;
-		v->ch_ams = 0;
-		v->ch_pms = 0;
-		v->ch_slot = 0x78;
-		v->ch_ne = 0;
-		for(int j = 0; j < 4; j++) {
-			struct opm_file_operator *o = v->operators + j;
-			o->ar = 0;
-			o->d1r = 0;
-			o->d2r = 0;
-			o->rr = 0;
-			o->d1l = 0;
-			o->tl = 127;
-			o->ks = 0;
-			o->mul = 1;
-			o->dt1 = 0;
-			o->dt2 = 0;
-			o->ame = 0;
-		}
-	}
+	memset(f, 0, sizeof(*f));
 }
 
 int opm_file_load(struct opm_file *opm, uint8_t *data, size_t data_len) {
-	memset(opm, 0, sizeof(struct opm_file));
 	if(data_len < 30) return -1;
 
 #define ALL_STATES \
@@ -69,47 +40,45 @@ ALL_STATES
 #define APPEND_PARAM { if(paramNum >= MAX_PARAMS) { fprintf(stderr, "Too many parameters at line %d\n", line); return -1; } else { digits[digitNum] = 0; params[paramNum++] = atoi(digits); digitNum = 0; } }
 #define APPEND_VOICE_NAME_CHAR { if(voiceNameLen >= MAX_VOICE_NAME) { fprintf(stderr, "Too many voice chars '%c' (%d) at line %d\n", CHAR_DOT(c), c, line); return -1; } else { voiceName[voiceNameLen++] = c; } }
 #define PARSE_PARAM { \
-	if(curVoice < OPM_FILE_MAX_VOICES && curVoice >= 0) { \
-		if(!strcmp(paramName, "LFO")) { \
-			if(paramNum != 5) { \
-				fprintf(stderr, "LFO parameters %d != 5\n", paramNum); \
-				return -1; \
-			} \
-			opm->voices[curVoice].lfo_lfrq = params[0]; \
-			opm->voices[curVoice].lfo_amd = params[1]; \
-			opm->voices[curVoice].lfo_pmd = params[2]; \
-			opm->voices[curVoice].lfo_wf = params[3]; \
-			opm->voices[curVoice].nfrq = params[4]; \
-		} else if(!strcmp(paramName, "CH")) { \
-			if(paramNum != 7) { \
-				fprintf(stderr, "CH parameters %d != 7\n", paramNum); \
-				return -1; \
-			} \
-			opm->voices[curVoice].ch_pan = params[0]; \
-			opm->voices[curVoice].ch_fl = params[1]; \
-			opm->voices[curVoice].ch_con = params[2]; \
-			opm->voices[curVoice].ch_ams = params[3]; \
-			opm->voices[curVoice].ch_pms = params[4]; \
-			opm->voices[curVoice].ch_slot = params[5]; \
-			opm->voices[curVoice].ch_ne = params[6]; \
-		} else if((paramName[0] == 'M' || paramName[0] == 'C') && (paramName[1] == '1' || paramName[1] == '2')) { \
-			if(paramNum != 11) { \
-				fprintf(stderr, "Operator %c%c parameters %d != 11\n", paramName[0], paramName[1], paramNum); \
-				return -1; \
-			} \
-			int opNum = paramName[0] == 'M' ? (paramName[1] == '1' ? 0 : 2) : (paramName[1] == '1' ? 1 : 3); \
-			opm->voices[curVoice].operators[opNum].ar = params[0]; \
-			opm->voices[curVoice].operators[opNum].d1r = params[1]; \
-			opm->voices[curVoice].operators[opNum].d2r = params[2]; \
-			opm->voices[curVoice].operators[opNum].rr = params[3]; \
-			opm->voices[curVoice].operators[opNum].d1l = params[4]; \
-			opm->voices[curVoice].operators[opNum].tl = params[5]; \
-			opm->voices[curVoice].operators[opNum].ks = params[6]; \
-			opm->voices[curVoice].operators[opNum].mul = params[7]; \
-			opm->voices[curVoice].operators[opNum].dt1 = params[8]; \
-			opm->voices[curVoice].operators[opNum].dt2 = params[9]; \
-			opm->voices[curVoice].operators[opNum].ame = params[10]; \
+	if(!strcmp(paramName, "LFO")) { \
+		if(paramNum != 5) { \
+			fprintf(stderr, "LFO parameters %d != 5\n", paramNum); \
+			return -1; \
 		} \
+		opm->voices[opm->num_voices-1].lfo_lfrq = params[0]; \
+		opm->voices[opm->num_voices-1].lfo_amd = params[1]; \
+		opm->voices[opm->num_voices-1].lfo_pmd = params[2]; \
+		opm->voices[opm->num_voices-1].lfo_wf = params[3]; \
+		opm->voices[opm->num_voices-1].nfrq = params[4]; \
+	} else if(!strcmp(paramName, "CH")) { \
+		if(paramNum != 7) { \
+			fprintf(stderr, "CH parameters %d != 7\n", paramNum); \
+			return -1; \
+		} \
+		opm->voices[opm->num_voices-1].ch_pan = params[0]; \
+		opm->voices[opm->num_voices-1].ch_fl = params[1]; \
+		opm->voices[opm->num_voices-1].ch_con = params[2]; \
+		opm->voices[opm->num_voices-1].ch_ams = params[3]; \
+		opm->voices[opm->num_voices-1].ch_pms = params[4]; \
+		opm->voices[opm->num_voices-1].ch_slot = params[5]; \
+		opm->voices[opm->num_voices-1].ch_ne = params[6]; \
+	} else if((paramName[0] == 'M' || paramName[0] == 'C') && (paramName[1] == '1' || paramName[1] == '2')) { \
+		if(paramNum != 11) { \
+			fprintf(stderr, "Operator %c%c parameters %d != 11\n", paramName[0], paramName[1], paramNum); \
+			return -1; \
+		} \
+		int opNum = paramName[0] == 'M' ? (paramName[1] == '1' ? 0 : 2) : (paramName[1] == '1' ? 1 : 3); \
+		opm->voices[opm->num_voices-1].operators[opNum].ar = params[0]; \
+		opm->voices[opm->num_voices-1].operators[opNum].d1r = params[1]; \
+		opm->voices[opm->num_voices-1].operators[opNum].d2r = params[2]; \
+		opm->voices[opm->num_voices-1].operators[opNum].rr = params[3]; \
+		opm->voices[opm->num_voices-1].operators[opNum].d1l = params[4]; \
+		opm->voices[opm->num_voices-1].operators[opNum].tl = params[5]; \
+		opm->voices[opm->num_voices-1].operators[opNum].ks = params[6]; \
+		opm->voices[opm->num_voices-1].operators[opNum].mul = params[7]; \
+		opm->voices[opm->num_voices-1].operators[opNum].dt1 = params[8]; \
+		opm->voices[opm->num_voices-1].operators[opNum].dt2 = params[9]; \
+		opm->voices[opm->num_voices-1].operators[opNum].ame = params[10]; \
 	} \
 }
 
@@ -122,7 +91,6 @@ ALL_STATES
 	int digitNum = 0;
 	int params[MAX_PARAMS + 1];
 	int paramNum = 0;
-	int curVoice = -1;
 	for(int i = 0; i < data_len; i++) {
 		uint8_t c = data[i];
 		//printf("line=%d c=%02x (%c) state=%d %s\n", line, c, c < 0x20 ? '.' : c, state, state_names[state]);
@@ -229,6 +197,7 @@ ALL_STATES
 				if(isspace(c)) {
 					// ignore
 				} else if(c == ':') {
+
 					state = AfterAt;
 				}
 				break;
@@ -245,16 +214,48 @@ ALL_STATES
 				}
 				break;
 			case InVoiceNum:
-				if(isspace(c)) {
-					digits[digitNum] = 0;
-					curVoice = atoi(digits);
-					if(curVoice > 255) curVoice = -1;
-					state = AfterVoiceNum;
-				} else if(isdigit(c)) {
+				if(isdigit(c)) {
 					APPEND_DIGIT;
 				} else {
-					APPEND_VOICE_NAME_CHAR;
-					state = InVoiceName;
+					digits[digitNum] = 0;
+					opm->num_voices++;
+					opm->voices = realloc(opm->voices, opm->num_voices * sizeof(opm->voices[0]));
+					if(!opm->voices) return -1;
+					struct opm_file_voice *v = opm->voices + opm->num_voices - 1;
+					v->number = atoi(digits);
+					v->lfo_lfrq = 0;
+					v->lfo_amd = 0;
+					v->lfo_pmd = 0;
+					v->lfo_wf = 0;
+					v->nfrq = 0;
+					v->ch_pan = 64;
+					v->ch_fl = 7;
+					v->ch_con = 4;
+					v->ch_ams = 0;
+					v->ch_pms = 0;
+					v->ch_slot = 0x78;
+					v->ch_ne = 0;
+					for(int j = 0; j < 4; j++) {
+						struct opm_file_operator *o = v->operators + j;
+						o->ar = 0;
+						o->d1r = 0;
+						o->d2r = 0;
+						o->rr = 0;
+						o->d1l = 0;
+						o->tl = 127;
+						o->ks = 0;
+						o->mul = 1;
+						o->dt1 = 0;
+						o->dt2 = 0;
+						o->ame = 0;
+					}
+
+					if(isspace(c)) {
+						state = AfterVoiceNum;
+					} else {
+						APPEND_VOICE_NAME_CHAR;
+						state = InVoiceName;
+					}
 				}
 				break;
 			case AfterVoiceNum:
@@ -268,64 +269,14 @@ ALL_STATES
 			case InVoiceName:
 				if(c == '\n') {
 					voiceName[voiceNameLen] = 0;
-					if(curVoice < OPM_FILE_MAX_VOICES && curVoice >= 0) {
-						opm->voices[curVoice].used = 1;
-						strncpy(opm->voices[curVoice].name, voiceName, voiceNameLen);
-						opm->voices[curVoice].name[voiceNameLen] = 0;
-					}
+					strncpy(opm->voices[opm->num_voices-1].name, voiceName, voiceNameLen);
+					opm->voices[opm->num_voices-1].name[voiceNameLen] = 0;
 					voiceNameLen = 0;
 					state = None;
 				} else {
 					APPEND_VOICE_NAME_CHAR;
 				}
 				break;
-		}
-	}
-
-	/* Mark "no Name" voices as unused */
-	for(int i = 0; i < OPM_FILE_MAX_VOICES; i++) {
-		struct opm_file_voice *v = &opm->voices[i];
-
-		/* Don't mark already unused voices as unused */
-		if(!v->used)
-			continue;
-
-		int unused = 1;
-
-		for(int k = 0; k < 4; k++) {
-			if(
-				v->operators[k].ar != 31 ||
-				v->operators[k].d1r != 0 ||
-				v->operators[k].d2r != 0 ||
-				v->operators[k].rr != 4 ||
-				v->operators[k].d1l != 0 ||
-				v->operators[k].tl != 0 ||
-				v->operators[k].ks != 0 ||
-				v->operators[k].mul != 1 ||
-				v->operators[k].dt1 != 0 ||
-				v->operators[k].dt2 != 0 ||
-				v->operators[k].ame != 0
-			) {
-				unused = 0;
-				break;
-			}
-		}
-
-		if(unused &&
-			v->lfo_lfrq == 0 &&
-			v->lfo_amd == 0 &&
-			v->lfo_pmd == 0 &&
-			v->lfo_wf == 0 &&
-			v->nfrq == 0 &&
-			v->ch_pan == 64 &&
-			v->ch_fl == 0 &&
-			v->ch_con == 0 &&
-			v->ch_ams == 0 &&
-			v->ch_pms == 0 &&
-			v->ch_slot == 64 &&
-			v->ch_ne == 0
-		) {
-			opm->voices[i].used = 0;
 		}
 	}
 
@@ -338,7 +289,7 @@ static const char *opm_file_operator_name(int i) {
 	return names[i];
 }
 
-int opm_file_save(struct opm_file *f, size_t (*write_fn)(void *buf, size_t bufsize, void *data_ptr), void *data_ptr) {
+int opm_file_save(struct opm_file *f, size_t (*write_fn)(void *buf, size_t bufsize, void *data_ptr), int pad_to, void *data_ptr) {
 	uint8_t buf[256];
 
 #define WRITEF(args...) { \
@@ -370,15 +321,15 @@ int opm_file_save(struct opm_file *f, size_t (*write_fn)(void *buf, size_t bufsi
 }
 
 void opm_file_dump(struct opm_file *f) {
-	for(int i = 0; i < OPM_FILE_MAX_VOICES; i++) {
+	for(int i = 0; i < f->num_voices; i++) {
 		struct opm_file_voice *v = f->voices + i;
-		printf("@:%d %s\n", i, v->name);
+		printf("@:%d %s\n", v->number, v->name);
 		printf("LFO: %d %d %d %d %d\n", v->lfo_lfrq, v->lfo_amd, v->lfo_pmd, v->lfo_wf, v->nfrq);
 		printf("CH: %d %d %d %d %d %d %d\n", v->ch_pan, v->ch_fl, v->ch_con, v->ch_ams, v->ch_pms, v->ch_slot, v->ch_ne);
 		printf("OP: AR D1R D2R RR D1L TL KS MUL DT1 DT2 AME\n");
 		for(int j = 0; j < 4; j++) {
 			struct opm_file_operator *o = v->operators + j;
-			printf("%s: %d %d %d %d %d %d %d %d %d %d %d\n", opm_file_operator_name(i), o->ar, o->d1r, o->d2r, o->rr, o->d1l, o->tl, o->ks, o->mul, o->dt1, o->dt2, o->ame);
+			printf("%s: %2d  %2d  %2d %2d  %2d %2d %2d  %2d  %2d  %2d   %d\n", opm_file_operator_name(j), o->ar, o->d1r, o->d2r, o->rr, o->d1l, o->tl, o->ks, o->mul, o->dt1, o->dt2, o->ame);
 		}
 	}
 }
--- opm_file.h
+++ opm_file.h
@@ -5,26 +5,28 @@
 #include <stdlib.h>
 
 #define OPM_FILE_MAX_VOICES 256
+#define OPM_FILE_MAX_NAME 256
 
 struct opm_file_operator {
 	uint8_t ar, d1r, d2r, rr, d1l, tl, ks, mul, dt1, dt2, ame;
 };
 
 struct opm_file_voice {
-	uint8_t used;
-	char name[OPM_FILE_MAX_VOICES];
+	int number;
+	char name[OPM_FILE_MAX_NAME];
 	uint8_t lfo_lfrq, lfo_amd, lfo_pmd, lfo_wf, nfrq;
 	uint8_t ch_pan, ch_fl, ch_con, ch_ams, ch_pms, ch_slot, ch_ne;
 	struct opm_file_operator operators[4];
 };
 
 struct opm_file {
-	struct opm_file_voice voices[OPM_FILE_MAX_VOICES];
+	struct opm_file_voice *voices;
+	int num_voices;
 };
 
 void opm_file_init(struct opm_file *f);
 int opm_file_load(struct opm_file *f, uint8_t *data, size_t data_len);
-int opm_file_save(struct opm_file *f, size_t (*write_fn)(void *buf, size_t bufsize, void *data_ptr), void *data_ptr);
+int opm_file_save(struct opm_file *f, size_t (*write_fn)(void *buf, size_t bufsize, void *data_ptr), int pad_to, void *data_ptr);
 void opm_file_dump(struct opm_file *f);
 
 #endif /* OPM_FILE_H_ */
